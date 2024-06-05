e7915bc607cddb4076d6a4cab6c51a09737cd892
ashan <uhashan17929@gmail.com>
2024-06-05 15:49:33 +0530

    Add My Folders and My Resume Folder with in download feature

--- src/components/Folders/MyResume.jsx
+++ src/components/Folders/MyResume.jsx
@@ -1,36 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Rnd } from 'react-rnd';
 import { XIcon, MinusIcon, PlusSmIcon } from '@heroicons/react/outline';
 
+import MyFilesIcon from '../Assets/Desktop/MyFile.png';
+import Chrome from '../Assets/Desktop/Chrome.png';
+import MyCvPDF from '../Assets/MyResume/MyCv.pdf'; // Correct relative path to MyCV
+import MyLetterPDF from '../Assets/MyResume/MyLetter.pdf'; // Correct relative path to Cover Letter
+
+
 const MyResume = ({ onClose }) => {
+  const [openWindows, setOpenWindows] = useState({
+    thisPC: false,
+    recycleBin: false,
+    chrome: false,
+    myResume: false,
+    aboutMe: false,
+    myProjects: false,
+    myFiles: false,
+  });
+
+  const handleOpen = (windowName) => {
+    setOpenWindows((prevState) => ({
+      ...prevState,
+      [windowName]: true,
+    }));
+  };
+
+  const handleClose = (windowName) => {
+    setOpenWindows((prevState) => ({
+      ...prevState,
+      [windowName]: false,
+    }));
+  };
+
   return (
     <Rnd
       default={{
-        x: 150,
-        y: 150,
-        width: 320,
-        height: 200,
+        x: 260,
+        y: 175,
+        width: 650,
+        height: 400,
       }}
-      minWidth={200}
-      minHeight={100}
+      minWidth={380}
+      minHeight={400}
       bounds="parent"
-      className="bg-[#202020] shadow-lg rounded-md border"
+      className="bg-[#202020] border-zinc-200 rounded-[10px] shadow-lg"
     >
-      <div className="flex justify-between items-center p-2 bg-[#2b2b2b] border-b cursor-move">
-        <div className="text-white ">My Resume</div>
-        <div className="flex space-x-2">
-          <button className="p-2 hover:bg-[#202020] rounded">
+      <div className="flex items-center justify-between p-2 bg-[#2b2b2b] rounded-t-[10px] border-b cursor-move">
+        <img className="h-[20px] w-[20px] bg-cover" src={MyFilesIcon} alt="This PC" />
+        <div className="flex text-white ">My Resume</div>
+        <div className="flex space-x-2 ">
+          <button className="p-2 rounded hover:bg-gray-500">
             <MinusIcon className="w-4 h-4 text-white" />
           </button>
-          <button className="p-2 hover:bg-[#202020] rounded">
+          <button className="p-2 rounded hover:bg-gray-500">
             <PlusSmIcon className="w-4 h-4 text-white" />
           </button>
-          <button onClick={onClose} className="p-2 hover:bg-[#E30D0D] rounded">
+          <button onClick={onClose} className="p-2 rounded hover:bg-[#eb342d]">
             <XIcon className="w-4 h-4 text-white" />
           </button>
         </div>
       </div>
-      <div className="p-4 text-white">This is bla bla buhshsjh</div>
+
+  <div className='flex items-center'>
+      <div className='flex flex-wrap flex-row p-[20px]  text-white'>
+        <div className="flex flex-col items-center">
+          <a href={MyCvPDF} download="MyCV.pdf"> {/* Link to MyCV PDF */}
+            <img className="h-[40px] w-[40px] bg-cover" src={Chrome} alt="My Files" />
+            <h1 className="mt-1 mb-[15px]">My CV</h1>
+          </a>
+        </div>
+
+        <div className="flex flex-col items-center ml-8 text-center ">
+          <a href={MyLetterPDF} download="MyLetter.pdf"> {/* Link to CoverLetter PDF */}
+            <img className="h-[40px] w-[40px] bg-cover" src={Chrome} alt="My Files" />
+            <h1 className="mt-1 mb-[15px]">Cover <br /> Letter</h1>
+          </a>
+        </div>
+      </div>
+      </div>
+      {/* End Section */}
+      <div className="flex items-end p-2 rounded-b-[10px] bg-[#2b2b2b] ">
+        <div className="flex text-white ">2 Items |</div>
+      </div>
+
     </Rnd>
   );
 };
--- src/components/Folders/ThisPC.jsx
+++ src/components/Folders/ThisPC.jsx
@@ -1,36 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Rnd } from 'react-rnd';
 import { XIcon, MinusIcon, PlusSmIcon } from '@heroicons/react/outline';
 
+import ThisPCIcon from '../Assets/Desktop/ThisPC.png';
+import DiskC from '../Assets/ThisPC/diskC.png';
+import DiskD from '../Assets/ThisPC/discD.png';
+import DiskE from '../Assets/ThisPC//discE.png';
+
 const ThisPC = ({ onClose }) => {
+
+  const [openWindows, setOpenWindows] = useState({
+    thisPC: false,
+    recycleBin: false,
+    chrome: false,
+    myResume: false,
+    aboutMe: false,
+    myProjects: false,
+    myFiles: false,
+  });
+
+  const handleOpen = (windowName) => {
+    setOpenWindows((prevState) => ({
+      ...prevState,
+      [windowName]: true,
+    }));
+  };
+
+  const handleClose = (windowName) => {
+    setOpenWindows((prevState) => ({
+      ...prevState,
+      [windowName]: false,
+    }));
+  };
+
+  
   return (
     <Rnd
       default={{
-        x: 200,
-        y: 200,
-        width: 320,
-        height: 200,
+        x: 250,
+        y: 150,
+        width: 850,
+        height: 400,
       }}
-      minWidth={200}
-      minHeight={100}
+      minWidth={380}
+      minHeight={400}
       bounds="parent"
-      className="bg-white border rounded-md shadow-lg"
+      className="bg-[#202020] border-zinc-200 rounded-[10px] shadow-lg"
     >
-      <div className="flex items-center justify-between p-2 bg-gray-200 border-b cursor-move">
-        <div className="text-gray-800">This PC</div>
-        <div className="flex space-x-2">
-          <button className="p-1 rounded hover:bg-gray-300">
-            <MinusIcon className="w-4 h-4 text-gray-800" />
+      <div className="flex items-center justify-between p-2 bg-[#2b2b2b] rounded-t-[10px] border-b cursor-move">
+      <img className="h-[20px] w-[20px] bg-cover" src={ThisPCIcon} alt="This PC" />
+        <div className="flex text-white ">This PC</div>
+        <div className="flex space-x-2 ">
+          <button className="p-2 rounded hover:bg-gray-500">
+            <MinusIcon className="w-4 h-4 text-white" />
           </button>
-          <button className="p-1 rounded hover:bg-gray-300">
-            <PlusSmIcon className="w-4 h-4 text-gray-800" />
+          <button className="p-2 rounded hover:bg-gray-500">
+            <PlusSmIcon className="w-4 h-4 text-white" />
           </button>
-          <button onClick={onClose} className="p-1 rounded hover:bg-gray-300">
-            <XIcon className="w-4 h-4 text-gray-800" />
+          <button onClick={onClose} className="p-2 rounded hover:bg-[#eb342d]">
+            <XIcon className="w-4 h-4 text-white" />
           </button>
         </div>
       </div>
-      <div className="p-4">This is the content of This PC window.</div>
+      
+      <div className='flex flex-wrap flex-row p-[20px] text-white'>
+      <div className="flex flex-col items-center" onDoubleClick={() => handleOpen('myFiles')}>
+            <img className="h-[40px] w-[200px] bg-cover" src={DiskC} alt="My Files" />
+            <h1 className="mt-1 mb-[15px]">Local Disk(C:)</h1>
+          </div>
+          <div className="flex flex-col items-center ml-8 text-end" onDoubleClick={() => handleOpen('myFiles')}>
+          <img className="h-[40px] w-[200px] bg-cover" src={DiskD} alt="My Files" />
+            <h1 className="mt-1 mb-[15px]">New Volume(D:)</h1>
+          </div>
+          <div className="flex flex-col items-center ml-8" onDoubleClick={() => handleOpen('myFiles')}>
+          <img className="h-[40px] w-[200px] bg-cover" src={DiskE} alt="My Files" />
+            <h1 className="mt-1 mb-[15px]">New Volume(E:)</h1>
+          </div>
+          </div>
+          {/* End Section */}
+          <div className="flex items-end p-2 rounded-b-[10px] bg-[#2b2b2b] ">
+        <div className="flex text-white ">3 Items |</div>
+      </div>
+
     </Rnd>
   );
 };
