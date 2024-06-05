f27049256af6f26c85d1341a5e53b25d7d93b2ba
ashan <uhashan17929@gmail.com>
2024-06-05 19:08:41 +0530

    Build Recycle bin

--- src/components/Desktop-Folders/RecycleBin.jsx
+++ src/components/Desktop-Folders/RecycleBin.jsx
@@ -2,35 +2,96 @@ import React from 'react';
 import { Rnd } from 'react-rnd';
 import { XIcon, MinusIcon, PlusSmIcon } from '@heroicons/react/outline';
 
+import MyFilesIcon from '../Assets/Desktop/MyFile.png';
+import ReactIcons from '../Assets/MyProject/reactIcon.png';
+import TrashBUCKET from '../Assets/Desktop/TrashBucket.png';
+
 const RecycleBin = ({ onClose }) => {
   return (
     <Rnd
       default={{
-        x: 200,
-        y: 200,
-        width: 320,
-        height: 200,
+        x: 250,
+        y: 150,
+        width: 850,
+        height: 400,
       }}
-      minWidth={200}
-      minHeight={100}
+      minWidth={380}
+      minHeight={400}
       bounds="parent"
-      className="bg-white border rounded-md shadow-lg"
+      className="bg-[#202020] border-[0.2px] border-[#4b4b4b] rounded-[10px] shadow-lg"
     >
-      <div className="flex items-center justify-between p-2 bg-gray-200 border-b cursor-move">
-        <div className="text-gray-800">Recycle Bin</div>
+      <div className="flex items-center justify-between p-2 bg-[#2b2b2b] rounded-t-[10px] border-b cursor-move">
+      <img className="h-[25px] w-[20px] bg-cover" src={TrashBUCKET} alt="This PC" />
+        <div className="flex text-white">Recycle Bin</div>
         <div className="flex space-x-2">
-          <button className="p-1 rounded hover:bg-gray-300">
-            <MinusIcon className="w-4 h-4 text-gray-800" />
+          <button className="p-2 rounded hover:bg-gray-500">
+            <MinusIcon className="w-4 h-4 text-white" />
           </button>
-          <button className="p-1 rounded hover:bg-gray-300">
-            <PlusSmIcon className="w-4 h-4 text-gray-800" />
+          <button className="p-2 rounded hover:bg-gray-500">
+            <PlusSmIcon className="w-4 h-4 text-white" />
           </button>
-          <button onClick={onClose} className="p-1 rounded hover:bg-gray-300">
-            <XIcon className="w-4 h-4 text-gray-800" />
+          <button onClick={onClose} className="p-2 rounded hover:bg-[#eb342d]">
+            <XIcon className="w-4 h-4 text-white" />
           </button>
         </div>
       </div>
-      <div className="p-4">This is the content of the Recycle Bin window.</div>
+
+      <div className='flex flex-wrap flex-row p-[20px] text-white'>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+          <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 01</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 02</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 03</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 04</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 05</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 06</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 07</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 08</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 09</h1>
+        </div>
+
+        <div className="flex flex-col items-center text-center pt-4 px-7 mt-[-5px] ml-[-5px] hover:bg-gray-500">
+        <img className="h-[60px] w-[60px] bg-cover" src={ReactIcons} alt="Disk C" />
+          <h1 className="mt-1 mb-[15px]">Project 10</h1>
+        </div>
+      </div>
+
+      <div className="flex items-end p-2 rounded-b-[10px] bg-[#2b2b2b]">
+        <div className="flex text-white">10 Items |</div>
+      </div>
     </Rnd>
   );
 };
